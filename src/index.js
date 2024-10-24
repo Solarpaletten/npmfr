802b0ddc299b611ef8aaae95cb27281f54876886
Solarpaletten <solarpaletten@gmail.com>
2024-10-25 00:49:34 +0300

    cors

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App'; // Импорт компонента App
 import reportWebVitals from './reportWebVitals'; // Импорт функции для сбора метрик
 
-
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
