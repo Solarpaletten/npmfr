66b29d4aa39df5f9f914a912f6376984c7f30360
Solarpaletten <solarpaletten@gmail.com>
2024-10-24 20:26:53 +0300

    localhost4000

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App'; // Импорт компонента App
 import reportWebVitals from './reportWebVitals'; // Импорт функции для сбора метрик
 
+
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
