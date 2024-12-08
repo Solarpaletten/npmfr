5b7b6f1eebcd44297750797669e88c0b6c5e3ac3
Solarpaletten <solarpaletten@gmail.com>
2024-12-08 19:23:31 +0100

    index bank operation

--- src/pages/Bank/BankOperations/service.js
+++ src/pages/Bank/BankOperations/service.js
@@ -4,12 +4,25 @@ const useBankOperationsService = () => {
   const api = useAuthenticatedApi();
 
   return {
+    // Get all operations
     getAll: () => {
-      return api.get('/bank-operations');
+      // Убираем https://npmbk.onrender.com из URL, так как это уже включено в BASE_URL
+      return api.get('/v1/bank/operations');
     },
 
+    // Create new operation
     create: (operationData) => {
-      return api.post('/bank-operations', operationData);
+      // Форматируем данные перед отправкой
+      const data = {
+        type: operationData.type,
+        amount: Number(operationData.amount),
+        date: operationData.date,
+        client: operationData.client,
+        description: operationData.description,
+        correspondingAccount: operationData.correspondingAccount
+      };
+      
+      return api.post('/v1/bank/operations', data);
     }
   };
 };
