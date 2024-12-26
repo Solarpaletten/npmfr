1766fd2d95d3791ace9f99c240e20393378e9790
Solarpaletten <solarpaletten@gmail.com>
2024-12-26 23:08:03 +0200

    Clients button

--- src/pages/Clients/columns.js
+++ src/pages/Clients/columns.js
@@ -1,11 +1,28 @@
 const columns = [
-  { key: "created_at", isSortable: false, label: "Registration date" },
-  { key: "name", isSortable: false, label: "Name" },
-  { key: "code", isSortable: false, label: "Code" },
-  { key: "vat_code", isSortable: false, label: "VAT code" },
-  { key: "phone", isSortable: false, label: "Phone number" },
-  { key: "email", isSortable: false, label: "Email" },
-  { key: "action", isSortable: false, label: "" },
+  {
+    title: "Registration date",
+    width: "120px"
+  },
+  {
+    title: "Name",
+    width: "200px"
+  },
+  {
+    title: "Code",
+    width: "120px"
+  },
+  {
+    title: "VAT code",
+    width: "120px" 
+  },
+  {
+    title: "Phone number",
+    width: "150px"
+  },
+  {
+    title: "Email",
+    width: "200px"
+  }
 ];
 
 export default columns;
